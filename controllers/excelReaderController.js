4cf0fd10ffd1bb78dab798a653dda58c306cc080
kosiaka <kos1234!>
2018-07-03 14:07:06 +0300

    Change Excel Table Data Position/Format

--- controllers/excelReaderController.js
+++ controllers/excelReaderController.js
@@ -8,11 +8,13 @@
 
 const Excel = require('exceljs');
 
-const createVocabulary = (worksheet) => {
+const createVocabulary = (worksheet, original = false) => {
     const vocabulary = {};
+    const column = original ? 2 : 3; // 2nd column - original; 3rd - translated
     worksheet.eachRow((row, rowNumber) => {
+        if (rowNumber === 1) return; // except titles
         let keyWord = row.getCell(1).value;
-        let translation = row.getCell(2).value;
+        let translation = row.getCell(column).value;
         vocabulary[keyWord] = translation;
     });
     return vocabulary;
@@ -24,11 +26,13 @@ const excelReaderController = async (pathToFile) => {
         let workbook = new Excel.Workbook();
         await workbook.xlsx.readFile(pathToFile);
         const vocabularies = {}; // [];
+        const worksheet_1 = workbook.getWorksheet(1);
+        vocabularies['Original'] = createVocabulary(worksheet_1, true);
         workbook.eachSheet((worksheet, sheetId) => {
             const {name: language} = worksheet;
             console.log('sheetId', sheetId);
             // console.log('worksheet', worksheet)
-            const vocabulary = createVocabulary(worksheet, language);
+            const vocabulary = createVocabulary(worksheet);
             // console.log(vocabulary);
             vocabularies[language] = vocabulary;
         });
